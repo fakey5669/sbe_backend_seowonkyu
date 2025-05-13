4fdedf01a65a0d5b12fec82df51616922dc6a1a4
unknown <fakey4477@gmail.com>
2025-05-13 16:54:05 +0900

    problem2 구조

--- problem2/src/server.ts
+++ problem2/src/server.ts
@@ -4,13 +4,13 @@ import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import logger from 'jet-logger';
 
-import BaseRouter from '@src/routes';
+import BaseRouter from './routes';
 
-import Paths from '@src/common/constants/Paths';
-import ENV from '@src/common/constants/ENV';
-import HttpStatusCodes from '@src/common/constants/HttpStatusCodes';
-import { RouteError } from '@src/common/util/route-errors';
-import { NodeEnvs } from '@src/common/constants';
+import Paths from './common/constants/Paths';
+import ENV from './common/constants/ENV';
+import HttpStatusCodes from './common/constants/HttpStatusCodes';
+import { RouteError } from './common/util/route-errors';
+import { NodeEnvs } from './common/constants';
 
 
 /******************************************************************************
@@ -66,14 +66,16 @@ app.set('views', viewsDir);
 const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
 
-// Nav to users pg by default
-app.get('/', (_: Request, res: Response) => {
-  return res.redirect('/users');
-});
-
-// Redirect to login if not logged in.
-app.get('/users', (_: Request, res: Response) => {
-  return res.sendFile('users.html', { root: viewsDir });
+// Nav to API info page by default
+// @ts-ignore - Express type issue
+app.get('/', (req, res) => {
+  return res.json({
+    message: '학교 급식 식단 정보 API 서비스',
+    endpoints: {
+      mealInfo: '/api/meals',
+      weeklyMealSummary: '/api/meals/weekly-summary'
+    }
+  });
 });
 
 
